1257ebacbc7e15b0f6749e070f224b5ef3e281d7
Khyosunny <kimhyo_0218@naver.com>
2021-11-14 00:14:09 +0900

    feat: enum color 추가

---
+++ src/styles.ts
@@ -0,0 +1,32 @@
+export enum EColors {
+  blue_100 = '#1B64DA',
+  blue_90 = '#2B7CFE',
+  blue_80 = '#478DFF',
+  blue_70 = '#53A2FF',
+  blue_60 = '#75BDFF',
+  blue_40 = '#B1DAFF',
+
+  black_100 = '#101010',
+  black_80 = '#5E5E5E',
+  black_60 = '#A5A5A5',
+  black_40 = '#C7C7C7',
+
+  gray_100 = '#D3D3D3',
+  gray_80 = '#EBEBEB',
+  gray_60 = '#F5F5F5',
+  gray_40 = '#FBFBFB',
+  white = '#fff',
+
+  red_100 = '#A70000',
+  red_80 = '#D11111',
+  red_60 = '#E54A4A',
+  red_40 = '#FF7272',
+
+  pink_20 = '#FCDDDD',
+
+  green_100 = '#0C9483',
+  green_80 = '#38B899',
+  green_60 = '#61D2B7',
+  green_40 = '#ADF1DD',
+  green_20 = '#D7F5EA',
+}
