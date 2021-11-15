eb1423bc55a76b4dc09512b4984a42e0f01b5d6c
Khyosunny <kimhyo_0218@naver.com>
2021-11-15 23:13:02 +0900

    refactor: button variant 타입 분리

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Global } from './GlobalStyle';
 export { default as Button } from './components/Button';
 
 export type TButtonSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type TButtonVariant = 'default' | 'outlined';
