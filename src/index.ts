a2e413ead1434d8b98087a9e4ba4dcc322bbf807
Khyosunny <kimhyo_0218@naver.com>
2021-12-07 00:59:59 +0900

    refactor: index.ts 경로 수정

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 export { default as Global } from './GlobalStyle';
-export * from './components';
+export { default as Button } from './components/Button';
+export { default as CheckBox } from './components/CheckBox';
+export { default as Icon } from './components/Icon/Icon';
+export { default as Input } from './components/Input';
+export { default as Radio } from './components/Radio';
+export { default as Tag } from './components/Tag';
+export { default as Text } from './components/Text';
+import * as icons from './components/Icon/Icons';
+export { icons };
 
 export type TButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 export type TButtonVariant = 'default' | 'outlined';
